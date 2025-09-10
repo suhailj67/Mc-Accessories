dec3b04727b7fc695b93e2d9b3d5bced1400ed5c
suhail <ebysuhail@gmail.com>
2025-09-10 19:46:55 +0000

    Fresh setup for Mc Accessories site

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-function showModal(productElem) {
-  document.getElementById('modal-img').src = productElem.querySelector('img').src;
-  document.getElementById('modal-title').textContent = productElem.querySelector('h3').textContent;
-  document.getElementById('modal-description').textContent = productElem.querySelector('p').textContent;
-  document.getElementById('modal-price').textContent = productElem.querySelector('.price').textContent;
-  document.getElementById('product-modal').classList.add('show');
-  document.body.style.overflow = 'hidden'; // Disable background scroll
+function scrollToProducts() {
+  document.getElementById("products").scrollIntoView({ 
+    behavior: "smooth" 
+  });
+}
+
+function openModal(imgSrc) {
+  let modal = document.getElementById("modal");
+  let modalImg = document.getElementById("modalImg");
+
+  modal.style.display = "block";
+  modalImg.src = imgSrc;
 }
 
 function closeModal() {
-  document.getElementById('product-modal').classList.remove('show');
-  document.body.style.overflow = 'auto';
+  let modal = document.getElementById("modal");
+  modal.style.display = "none";
 }
-
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    closeModal();
-  }
-});
